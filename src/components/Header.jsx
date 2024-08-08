fdb3335af59773394338c5d554be3282f7cac64e
Akshay Sunil <akshayofficial43@gmail.com>
2024-08-08 19:39:36 +0530

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,7 @@ import { Download, DownloadIcon } from "lucide-react";
 function Header({ DownloadIcon }) {
   return (
     <div className="p-4 shadow-sm border flex justify-between items-center">
-      <img src="./logo.svg" alt="..." w-10px h-4px />
+      <img src="/assets/logo.svg" alt="..." />
       <Button
         className="flex gap-2 items-center"
         onClick={() => DownloadIcon(Date.now())}
